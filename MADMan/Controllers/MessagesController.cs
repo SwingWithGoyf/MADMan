097f63fb80de2d0413127ac86bffb5b7c3bc6be1
Jeremy Elgar <jelgar@microsoft.com>
2018-07-23 07:18:34 -0700

    Updating to use new client-side bot template

--- MADMan/Controllers/MessagesController.cs
+++ MADMan/Controllers/MessagesController.cs
@@ -1,59 +1,57 @@
+﻿using System.Net;
+using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web.Http;
-
-using Microsoft.Bot.Connector;
 using Microsoft.Bot.Builder.Dialogs;
-using System.Web.Http.Description;
-using System.Net.Http;
+using Microsoft.Bot.Connector;
 
-namespace Microsoft.Bot.Sample.SimpleEchoBot
+namespace MADMan
 {
     [BotAuthentication]
     public class MessagesController : ApiController
     {
         /// <summary>
         /// POST: api/Messages
-        /// receive a message from a user and send replies
+        /// Receive a message from a user and reply to it
         /// </summary>
-        /// <param name="activity"></param>
-        [ResponseType(typeof(void))]
-        public virtual async Task<HttpResponseMessage> Post([FromBody] Activity activity)
+        public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
         {
-            // check if activity is of type message
-            if (activity != null && activity.GetActivityType() == ActivityTypes.Message)
+            if (activity.GetActivityType() == ActivityTypes.Message)
             {
-                await Conversation.SendAsync(activity, () => new EchoDialog());
+                await Conversation.SendAsync(activity, () => new Dialogs.RootDialog());
             }
             else
             {
                 HandleSystemMessage(activity);
             }
-            return new HttpResponseMessage(System.Net.HttpStatusCode.Accepted);
+            var response = Request.CreateResponse(HttpStatusCode.OK);
+            return response;
         }
 
         private Activity HandleSystemMessage(Activity message)
         {
-            if (message.Type == ActivityTypes.DeleteUserData)
+            string messageType = message.GetActivityType();
+            if (messageType == ActivityTypes.DeleteUserData)
             {
                 // Implement user deletion here
                 // If we handle user deletion, return a real message
             }
-            else if (message.Type == ActivityTypes.ConversationUpdate)
+            else if (messageType == ActivityTypes.ConversationUpdate)
             {
                 // Handle conversation state changes, like members being added and removed
                 // Use Activity.MembersAdded and Activity.MembersRemoved and Activity.Action for info
                 // Not available in all channels
             }
-            else if (message.Type == ActivityTypes.ContactRelationUpdate)
+            else if (messageType == ActivityTypes.ContactRelationUpdate)
             {
                 // Handle add/remove from contact lists
                 // Activity.From + Activity.Action represent what happened
             }
-            else if (message.Type == ActivityTypes.Typing)
+            else if (messageType == ActivityTypes.Typing)
             {
-                // Handle knowing tha the user is typing
+                // Handle knowing that the user is typing
             }
-            else if (message.Type == ActivityTypes.Ping)
+            else if (messageType == ActivityTypes.Ping)
             {
             }
 
--- publish.cmd
+++
@@ -1,3 +0,0 @@
-nuget restore
-msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=madman-Web-Deploy.pubxml -p:Password=18tvbKzg1WM3963fFLTLjnxuhJyk1Ns16AdvYml5EeJCvMsNFdEoGtkfs5bg
-
